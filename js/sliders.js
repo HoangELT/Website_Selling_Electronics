168af04feb0786be17b21bcf935f139d4c6546a5
HoangELT <dovanhoang0505@gmail.com>
2023-04-09 09:21:20 +0700

    update indext, img product,...

--- js/sliders.js
+++ js/sliders.js
@@ -22,12 +22,3 @@ function slideRun (index) {
 }
 
 setInterval (slider,5000)
-
-const toP = document.querySelector(".top")
-window.addEventListener("scroll",function(){
-   const X = this.pageYOffset;
- if(X>1){toP.classList.add("active")}
- else {
-     toP.classList.remove("active")
- }
-}) 
