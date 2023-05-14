d15de51977db5170fa04544c269ffc99ad0c4a59
HoangELT <dovanhoang0505@gmail.com>
2023-05-14 18:53:59 +0700

    update moi

--- js/slidecart.js
+++
@@ -1,6 +0,0 @@
-const itemsliderbar = document.querySelectorAll(".cartegory-left-li")
-itemsliderbar.forEach(function (menu, index) {
-  menu.addEventListener("click", function () {
-    menu.classList.toggle("block")
-  })
-})
